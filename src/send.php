94846bd47d6aef5bbe53efa456d4d6194804e395
JC Camargo <jccamargo15@gmail.com>
2018-10-15 16:42:42 -0300

    send.php segmentado

--- src/send.php
+++ src/send.php
@@ -5,42 +5,7 @@ require_once 'fill_fields.php';
 
 require_once 'fill_file.php';
 
-/**
- * Dados e-mail que chega para você
- */
-$para = $meu_email;
-$assunto_contato = "Contato enviado pelo site: $assunto";
-$header = "
-<b>Nome:</b>    $nome ($empresa),<br>
-<b>Email:</b>   $email<br>
-<b>Assunto:</b> $assunto<br>
-<br><br>
-<b>Mensagem:</b><br>
-$mensagem
-<br>
-<b>Link para download do anexo:</b> $link_arquivo<br>
-<br><br>
-Data: $data";
-$headers = "MIME-Version: 1.0\r\n";
-$headers .= "Content-type: text/html;charset=utf-8\r\n";
-$headers .= "From: $nome <$email>\r\n";
-
-/**
- * Dados e-mail de resposta que vai para o cliente/visitante
- */
-$resp_assunto = $assunto_resposta;
-$header2 = "
-Olá <b>$nome</b>,
-<br><br>
-Obrigado pelo seu contato.<br>
-Recebemos sua mensagem e brevemente entraremos em contato.
-<br><br><br>
-Atenciosamente,<br>
-$empresa<br>";
-
-$headers2 = "MIME-Version: 1.0\r\n";
-$headers2 .= "Content-type: text/html; charset=utf-8\r\n";
-$headers2 .= "From: $empresa <$meu_email>\r\n";
+require_once 'write_mail.php';
 
 /**
  * Caso $debug seja verdade, imprime todos os dados
